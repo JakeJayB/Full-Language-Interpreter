55d202e77a9ea3ea932bda17b4123bae4beb4e87
Jacob Bejarano <jacobjbejarano@gmail.com>
2023-03-04 17:26:32 -0800

    edited Parser

--- Parser.py
+++ Parser.py
@@ -1,3 +1,5 @@
+import argparse
+
 class Node():
     def __init__(self, value) -> None:
         self.value = value
@@ -9,7 +11,7 @@ class Tree():
     def __init__(self, root) -> None:
         self.root = root
     
-    def MakeSubTree(self, root, left, middle, right):
+    def MakeSubTree(self, root, left=None, middle=None, right=None):
         root = Node(root.value)
         root.left = left
         root.middle = middle
@@ -19,9 +21,28 @@ class Tree():
         
 def printTree(node, whitespace):
     if node == None: return
-    print(node.value)
+    print(whitespace + str(node.value))
     printTree(node.left, whitespace+'\t')
     printTree(node.middle, whitespace+'\t')
     printTree(node.right, whitespace+'\t')
     
+def ArgParser():
+    """
+        Arugment Parser for the arguments passed from command line
         
+        @return: None
+    """
+    argParser = argparse.ArgumentParser()
+    argParser.add_argument("-i", "--input", help="Input File")
+    argParser.add_argument("-o", "--output", help="Output File")
+    args = argParser.parse_args()
+
+    input = open(args.input, "r")
+    output = open(args.output, "a")
+    
+    main(input, output)
+    
+def main(input, output):
+    pass
+        
+ArgParser()
